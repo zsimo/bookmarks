64cbbec02ef9d09585409b2003072e4321f7ae13
zsimo <zsimo30@gmail.com>
2014-07-05 16:09:30 +0200

    ...

--- php/locker.php
+++ php/locker.php
@@ -1,33 +1,40 @@
 <?php
-	
+
+	require 'lib/password.php';
+
+	$storedPass = '$2y$10$/vSfE2GH.QZEzfjDH3Y/E.SBoQ.3WKrklbTBCZNs2ixZCOatGXAXi';
+
 	$name = '';
 	$pass = '';
+	$cryptoPass = '';
 	$today = '';
 	$status = 'error';
 	$file = '../lock.json';
-	
-	
+
+
 	if (isset($_POST['name'])) {
 		$name = $_POST['name'];
 	}
 	if (isset($_POST['pass'])) {
 		$pass = $_POST['pass'];
+		// $passToBeEventuallyStored = password_hash($pass, PASSWORD_BCRYPT);
 	}
 	if (isset($_POST['today'])) {
 		$today = $_POST['today'];
 	}
-	
+
 	if ($today == 'lock') {
 		if (file_put_contents($file, 'var todayLock = "'.$today.'";')) {
 			$status = 'locked';
 		}
 	}
 
-	if ($pass == 'banzai') {
-			if (file_put_contents($file, 'var todayLock = "'.$today.'";')) {
-				$status = 'ok';
-			}
+	if (password_verify($pass, $storedPass)) {
+	// if (crypt($pass, $cryptoPass) == $cryptoPass) {
+		if (file_put_contents($file, 'var todayLock = "'.$today.'";')) {
+			$status = 'ok';
 		}
+	}
 
 
 
@@ -41,7 +48,8 @@ header('Content-type: application/json');
 
 	echo '"locker" : "'.$status.'",';
 	echo '"today" : "'.$today.'",';
-
+	echo '"pass" : "'.$pass.'",';
+	echo '"cryptoPass" : "'.$cryptoPass.'",';
 
 
 	echo '"last": ""';
